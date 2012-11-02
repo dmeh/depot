31dad58edd5e692f674fa2d8771d203280c194d2
Dean Mehinovic <dmehinovic@market76.com>
2012-11-02 16:15:52 -0400

    deleted public/index.html

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png|jpeg)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
+    message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
   }
+  validates :title, length: {minimum: 10}
 end
